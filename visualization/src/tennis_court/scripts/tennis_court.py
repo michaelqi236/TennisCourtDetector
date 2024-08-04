75b62dcc430846cbd4c171501cf6ba29e59fd961
Michael Qi <michaelqi236@gmail.com>
2024-08-04 21:30:23 +0000

    rviz works

--- visualization/src/tennis_court/scripts/tennis_court.py
+++ visualization/src/tennis_court/scripts/tennis_court.py
@@ -9,14 +9,13 @@ import geometry_msgs.msg
 import sys
 import os
 
-
 dir_path = os.path.dirname(os.path.realpath(__file__))
-path = os.sep.join(dir_path.split(os.sep)[:3])
+path_layers =  dir_path.split(os.sep)[:3]
+path_layers.append('lib')
+path = os.sep.join(path_layers)
 sys.path.append(path)
 
-from lib import parameters
-
-exit()
+from parameters import CourtParam
 
 def init_rviz():
     rospy.init_node('tennis_court_visualization')
@@ -71,7 +70,7 @@ def create_line_marker(marker_id, points, color, frame_id="world", lifetime=0):
     marker.lifetime = rospy.Duration(lifetime)
     return marker
 
-def create_cube_marker(marker_id, center, shape, color, frame_id="world", lifetime=0):
+def create_cube_marker(marker_id, center, shape, color, frame_id="world", z_offset = 0):
     marker = Marker()
     marker.header.frame_id = frame_id
     marker.header.stamp = rospy.Time.now()
@@ -92,9 +91,8 @@ def create_cube_marker(marker_id, center, shape, color, frame_id="world", lifeti
     
     marker.pose.position.x = center[0]
     marker.pose.position.y = center[1]
-    marker.pose.position.z = -0.005
+    marker.pose.position.z = z_offset
 
-    marker.lifetime = rospy.Duration(lifetime)
     return marker
 
 
@@ -102,48 +100,58 @@ def draw_court(marker_pub):
     court_param = CourtParam()
 
     # Court ground
-    ground_marker = create_cube_marker(0, [0,0,0], shape, [0, 1, 0, 1], frame_id="world", lifetime=0)# Green color
- 
-    # Court boundaries
-    boundary_points = [
-        Point(0, 0, 0), Point(court_width, 0, 0), Point(court_width, court_length, 0),
-        Point(0, court_length, 0), Point(0, 0, 0)
-    ]
-    boundary_marker = create_line_marker(0, boundary_points, [0, 1, 0, 1], lifetime=0)  
-    # Net
-    net_points = [Point(0, court_length / 2, 0), Point(court_width, court_length / 2, 0)]
-    net_marker = create_line_marker(1, net_points, [0, 0, 0, 1], lifetime=0)  # Black color
+    ground_marker_0 = create_cube_marker(0, center=court_param.net_points[5], shape=[court_param.width, court_param.length], color=[0, 1, 0, 1], frame_id="world", z_offset = -0.01)# Green color
+    buffer = 5
+    ground_marker_1 = create_cube_marker(1, center=court_param.net_points[5], shape=[court_param.width + buffer, court_param.length + buffer], color=[0.9, 0.5, 0, 1], frame_id="world", z_offset = -0.03)# yellow color
 
-    # Service boxes
-    service_line_distance = 6.40
-    service_box_points_1 = [
-        Point(0, service_line_distance, 0), Point(court_width, service_line_distance, 0)
-    ]
-    service_box_points_2 = [
-        Point(0, court_length - service_line_distance, 0), Point(court_width, court_length - service_line_distance, 0)
-    ]
-    center_line_points = [Point(court_width / 2, 0, 0), Point(court_width / 2, court_length, 0)]
-    service_box_marker_1 = create_line_marker(2, service_box_points_1, [0, 1, 0, 1], lifetime=0)  # Green color
-    service_box_marker_2 = create_line_marker(3, service_box_points_2, [0, 1, 0, 1], lifetime=0)  # Green color
-    center_line_marker = create_line_marker(4, center_line_points, [0, 1, 0, 1], lifetime=0)  # Green color
+    # Court boundaries
+    court_points = court_param.court_points
+    net_points = court_param.net_points
+    
+    point_idx_0 = [0,1,3,2,0] # court boundary
+    line_points_0 = [Point(court_points[i][0], court_points[i][1], 0) for i in  point_idx_0]
+    line_marker_0 = create_line_marker(0, line_points_0, [1, 1, 1, 1], lifetime=0)  # White color
+
+    point_idx_1 = [4,5] # inner court boundary
+    line_points_1 = [Point(court_points[i][0], court_points[i][1], 0) for i in  point_idx_1]
+    line_marker_1 = create_line_marker(1, line_points_1, [1, 1, 1, 1], lifetime=0)  # White color
+
+    point_idx_2 = [6,7] # inner court boundary
+    line_points_2 = [Point(court_points[i][0], court_points[i][1], 0) for i in  point_idx_2]
+    line_marker_2 = create_line_marker(2, line_points_2, [1, 1, 1, 1], lifetime=0)  # White color
+
+    point_idx_3 = [8,9] # serving box boundary
+    line_points_3 = [Point(court_points[i][0], court_points[i][1], 0) for i in  point_idx_3]
+    line_marker_3 = create_line_marker(3, line_points_3, [1, 1, 1, 1], lifetime=0)  # White color
+
+    point_idx_4 = [10,11] # serving box boundary
+    line_points_4 = [Point(court_points[i][0], court_points[i][1], 0) for i in  point_idx_4]
+    line_marker_4 = create_line_marker(4, line_points_4, [1, 1, 1, 1], lifetime=0)  # White color
+
+    point_idx_5 = [12,13] # mid line
+    line_points_5 = [Point(court_points[i][0], court_points[i][1], 0) for i in  point_idx_5]
+    line_marker_5 = create_line_marker(5, line_points_5, [1, 1, 1, 1], lifetime=0)  # White color
+
+    point_idx_6 = [7,9] # net line
+    line_points_6 = [Point(net_points[i][0], net_points[i][1], 0) for i in  point_idx_6]
+    line_marker_6 = create_line_marker(6, line_points_6, [1, 1, 1, 1], lifetime=0)  # White color
 
     rate = rospy.Rate(1)  # 1 Hz
     while not rospy.is_shutdown():
         rospy.loginfo("Publishing markers...")
-        marker_pub.publish(ground_marker)
-        marker_pub.publish(boundary_marker)
-        marker_pub.publish(net_marker)
-        marker_pub.publish(service_box_marker_1)
-        marker_pub.publish(service_box_marker_2)
-        marker_pub.publish(center_line_marker)
+        marker_pub.publish(ground_marker_1)
+        marker_pub.publish(ground_marker_0)
+        marker_pub.publish(line_marker_0)
+        marker_pub.publish(line_marker_1)
+        marker_pub.publish(line_marker_2)
+        marker_pub.publish(line_marker_3)
+        marker_pub.publish(line_marker_4)
+        marker_pub.publish(line_marker_5)
+        marker_pub.publish(line_marker_6)
         rate.sleep()
 
 if __name__ == '__main__':
-    try:
-        init_rviz()
+    init_rviz()
 
-        marker_pub = rospy.Publisher('visualization_marker', Marker, queue_size=10)
-        draw_court(marker_pub)
-
-    except rospy.ROSInterruptException:
-        pass
+    marker_pub = rospy.Publisher('visualization_marker', Marker, queue_size=10)
+    draw_court(marker_pub)
